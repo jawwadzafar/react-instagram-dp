097f45546a911ebe5f3d371f68fc81fffe3f8726
Jawwad Zafar <zafarjawwad@gmail.com>
2019-07-30 08:30:10 +0530

    Updates

---
+++ precache-manifest.1cae2b7fc0f212b161c674c0ca912844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1f5519189e7c8aff5140",
+    "url": "/static/js/main.cb3330a7.chunk.js"
+  },
+  {
+    "revision": "ac6c3f7920ddd542dd15",
+    "url": "/static/js/2.cf38e645.chunk.js"
+  },
+  {
+    "revision": "1f5519189e7c8aff5140",
+    "url": "/static/css/main.17ecf4db.chunk.css"
+  },
+  {
+    "revision": "60457cac6ac7884b5b8e439cf715fd54",
+    "url": "/index.html"
+  }
+];
