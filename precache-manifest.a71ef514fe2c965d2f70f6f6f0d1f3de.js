41a7f985a8ce05279e9ec0f49122742404644388
Jawwad Zafar <zafarjawwad@gmail.com>
2019-08-01 23:15:07 +0530

    Updates

---
+++ precache-manifest.a71ef514fe2c965d2f70f6f6f0d1f3de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5c96f50d4fb31a5dc99",
+    "url": "/react-instagram-dp/static/js/runtime~main.9c539b0a.js"
+  },
+  {
+    "revision": "bde473d927e28e9a701c",
+    "url": "/react-instagram-dp/static/js/main.d7dafc42.chunk.js"
+  },
+  {
+    "revision": "ac6c3f7920ddd542dd15",
+    "url": "/react-instagram-dp/static/js/2.cf38e645.chunk.js"
+  },
+  {
+    "revision": "bde473d927e28e9a701c",
+    "url": "/react-instagram-dp/static/css/main.17ecf4db.chunk.css"
+  },
+  {
+    "revision": "79f0c8f56eeab8d6010a0a0a60940a0d",
+    "url": "/react-instagram-dp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c8b1e7256492e0bedbe43d58f0c3965.js"
+  "/react-instagram-dp/precache-manifest.a71ef514fe2c965d2f70f6f6f0d1f3de.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-instagram-dp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
