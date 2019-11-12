beb245df1c2d4e00904b437bfdad07d2926f0359
Aamir khan <aamirkhan180@gmail.com>
2019-11-12 16:30:23 +0530

    refactoring

--- src/components/ImageBox.js
+++ src/components/ImageBox.js
@@ -1,18 +1,26 @@
 import React from "react";
 import Emoji from "./Emoji";
 
-const ImageBox = ({ fullName, img, followers, following, bio }) => {
+const ImageBox = ({ username, fullName, img, followers, following, bio }) => {
   return (
-    <div className="profileBox">
-      <a href={img} target="_blank" rel="noopener noreferrer">
-        <img src={img} alt={`profile pic of ${fullName}`} className="profileBox_img" />
+    <section className="profileBox">
+      <a
+        href={`https://www.instagram.com/${username}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={img}
+          alt={`profile pic of ${fullName}`}
+          className="profileBox_img"
+        />
       </a>
       <p>
         <a href={img} target="_blank" rel="noopener noreferrer">
           <Emoji symbol="👉" /> View In full size
         </a>
       </p>
-      <strong>{fullName}</strong>
+      <h1 className="profileBox_fullName">{fullName}</h1>
       {bio && <p className="profileBox_bio">{bio}</p>}
       <ul>
         <li>
@@ -22,7 +30,7 @@ const ImageBox = ({ fullName, img, followers, following, bio }) => {
           <span className="bold">{following}</span> following
         </li>
       </ul>
-    </div>
+    </section>
   );
 };
 
